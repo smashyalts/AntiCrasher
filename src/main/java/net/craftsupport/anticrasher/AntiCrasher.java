a8a0b536726c3a4d6cd2cd14496bc1c26d3547e4
david <smashyalts@gmail.com>
2023-11-05 15:44:15 +0100

    Source fix

--- src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
+++ src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
@@ -1,17 +1,36 @@
 package net.craftsupport.anticrasher;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import net.craftsupport.anticrasher.packet.packetstuff;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClickWindow;
 public final class AntiCrasher extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        //Are all listeners read only?
+        PacketEvents.getAPI().getSettings().reEncodeByDefault(false)
+                .checkForUpdates(true)
+                .bStats(true);
+        PacketEvents.getAPI().load();
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        PacketEvents.getAPI().getEventManager().registerListener(new packetstuff(),
+                PacketListenerPriority.LOW);
+        PacketEvents.getAPI().init();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        PacketEvents.getAPI().terminate();
     }
 }
--- src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
+++ src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
@@ -1,2 +1,22 @@
-package net.craftsupport.anticrasher;public class packetstuff {
+package net.craftsupport.anticrasher.packet;
+
+import com.github.retrooper.packetevents.event.PacketListener;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClickWindow;
+
+public class packetstuff implements PacketListener {
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (event.getPacketType() == PacketType.Play.Client.CLICK_WINDOW) {
+            WrapperPlayClientClickWindow click = new WrapperPlayClientClickWindow(event);
+            int clickType = click.getWindowClickType().ordinal();
+            int button = click.getButton();
+            int windowId = click.getWindowId();
+
+            if ((clickType == 1 || clickType == 2) && windowId >= 0 && button < 0) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
 }
