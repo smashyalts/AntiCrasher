a56809ad75067a09811e5768b7103a487c853ac8
david <smashyalts@gmail.com>
2023-11-05 21:39:04 +0100

    Update 1.2

--- src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
+++ src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
@@ -2,16 +2,17 @@ package net.craftsupport.anticrasher;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import io.github.retrooper.packetevents.bstats.Metrics;
 import net.craftsupport.anticrasher.packet.packetstuff;
-import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
-import com.github.retrooper.packetevents.event.PacketReceiveEvent;
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClickWindow;
+
 public final class AntiCrasher extends JavaPlugin {
+    static Plugin plugin;
     @Override
     public void onLoad() {
+
         PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
         //Are all listeners read only?
         PacketEvents.getAPI().getSettings().reEncodeByDefault(false)
@@ -22,12 +23,14 @@ public final class AntiCrasher extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        int pluginId = 20218;
+        Metrics metrics = new Metrics(this, pluginId);
         // Plugin startup logic
         PacketEvents.getAPI().getEventManager().registerListener(new packetstuff(),
                 PacketListenerPriority.LOW);
         PacketEvents.getAPI().init();
     }
-
     @Override
     public void onDisable() {
         // Plugin shutdown logic
--- src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
+++ src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
@@ -4,8 +4,16 @@ import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClickWindow;
+import net.craftsupport.anticrasher.AntiCrasher;
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
+import sun.util.calendar.BaseCalendar;
+
+import java.util.Date;
 
 public class packetstuff implements PacketListener {
+    BanList banlist = Bukkit.getBanList(BanList.Type.NAME);
+    BanList banlistip = Bukkit.getBanList(BanList.Type.IP);
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == PacketType.Play.Client.CLICK_WINDOW) {
             WrapperPlayClientClickWindow click = new WrapperPlayClientClickWindow(event);
@@ -15,8 +23,18 @@ public class packetstuff implements PacketListener {
 
             if ((clickType == 1 || clickType == 2) && windowId >= 0 && button < 0) {
                 event.setCancelled(true);
+                if (AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getBoolean("log-attempts")) {
+                Bukkit.getLogger().warning(event.getUser().getName() + " Tried to use the Crash Exploit");
+                }
+if (AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getBoolean("ban-on-attempt")) {
+    if (AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getBoolean("ip-ban")) {
+        banlistip.addBan(event.getUser().getAddress().toString(), AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getString("ban-on-attempt-message"), null, "AntiCrasher");
+    }
+    else banlist.addBan(event.getUser().getName(), AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getString("ban-on-attempt-message"), null, "AntiCrasher");
+}
+
+                }
             }
         }
     }
 
-}
