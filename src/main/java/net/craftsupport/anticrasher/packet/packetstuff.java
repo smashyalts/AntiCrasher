3122d7d82f55f6bde144c634228dc4150e68f794
david <smashyalts@gmail.com>
2023-11-06 18:27:55 +0100

    Update 1.3.2

--- src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
+++ src/main/java/net/craftsupport/anticrasher/packet/packetstuff.java
@@ -15,8 +15,9 @@ public class packetstuff implements PacketListener {
             int clickType = click.getWindowClickType().ordinal();
             int button = click.getButton();
             int windowId = click.getWindowId();
+            int slot = click.getSlot();
 
-            if ((clickType == 1 || clickType == 2) && windowId >= 0 && button < 0) {
+             if ((clickType == 1 || clickType == 2) && windowId >= 0 && (slot < 0 || button < 0)) {
                 event.setCancelled(true);
                 if (AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getBoolean("log-attempts")) {
                     Bukkit.getLogger().warning(event.getUser().getName() + " Tried to use the Crash Exploit");
@@ -24,7 +25,9 @@ public class packetstuff implements PacketListener {
                 if (AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getBoolean("punish-on-attempt")) {
                     String ReplacedString = AntiCrasher.getPlugin(AntiCrasher.class).getConfig().getString("punish-command").replaceAll("%player%", event.getUser().getName());
                     Bukkit.getScheduler().runTask(AntiCrasher.getPlugin(AntiCrasher.class), () -> {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(event.getUser().getUUID()), ReplacedString));
+                        if (Bukkit.getServer().getPluginManager().getPlugin("PlaceholderAPI").isEnabled()) {Bukkit.dispatchCommand(Bukkit.getConsoleSender(), PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(event.getUser().getUUID()), ReplacedString));
+                        }
+                        else {Bukkit.dispatchCommand(Bukkit.getConsoleSender(), ReplacedString);}
                     });
                 }
             }
