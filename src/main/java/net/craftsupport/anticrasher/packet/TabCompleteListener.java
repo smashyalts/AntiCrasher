571371173fe5f717c91344acde3e9f54ea5fd6df
MyoWalE <161393432+MyoWalE@users.noreply.github.com>
2024-03-02 12:43:54 +0900

    Update TabCompleteListener.java

--- src/main/java/net/craftsupport/anticrasher/packet/TabCompleteListener.java
+++ src/main/java/net/craftsupport/anticrasher/packet/TabCompleteListener.java
@@ -24,20 +24,22 @@ public class TabCompleteListener implements PacketListener {
 
     @Override
     public void onPacketReceive(PacketReceiveEvent event) {
-        if (event.getPacketType() == PacketType.Play.Client.TAB_COMPLETE) {
-            WrapperPlayClientTabComplete wrapper = new WrapperPlayClientTabComplete(event);
-            String text = wrapper.getText();
-            final int length = text.length();
-            Player player = (Player) event.getPlayer();
+        if (plugin.getConfig().getBoolean("tabcompletion-exploit")) {
+            if (event.getPacketType() == PacketType.Play.Client.TAB_COMPLETE) {
+                WrapperPlayClientTabComplete wrapper = new WrapperPlayClientTabComplete(event);
+                String text = wrapper.getText();
+                final int length = text.length();
+                Player player = (Player) event.getPlayer();
 
-            // general length limit
-            if (length > 256 && !player.hasPermission("anticrasher.bypass")) {
-                handleInvalidPacket(event);
-            }
-            // paper's patch
-            final int index;
-            if (text.length() > 64 && ((index = text.indexOf(' ')) == -1 && !player.hasPermission("anticrasher.bypass") || index >= 64 && !player.hasPermission("anticrasher.bypass"))) {
-                handleInvalidPacket(event);
+                // general length limit
+                if (length > 256 && !player.hasPermission("anticrasher.bypass")) {
+                    handleInvalidPacket(event);
+                }
+                // paper's patch
+                final int index;
+                if (text.length() > 64 && ((index = text.indexOf(' ')) == -1 && !player.hasPermission("anticrasher.bypass") || index >= 64 && !player.hasPermission("anticrasher.bypass"))) {
+                    handleInvalidPacket(event);
+                }
             }
         }
     }
