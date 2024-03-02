5b31cd7b7ed8d635b9767e1244cc8829d224bcf7
MyoWalE <161393432+MyoWalE@users.noreply.github.com>
2024-03-02 15:41:50 +0900

    Update AntiCrasher.java

--- src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
+++ src/main/java/net/craftsupport/anticrasher/AntiCrasher.java
@@ -2,6 +2,9 @@ package net.craftsupport.anticrasher;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
+import com.jeff_media.updatechecker.UserAgentBuilder;
 import io.github.retrooper.packetevents.bstats.Metrics;
 import net.craftsupport.anticrasher.packet.TabCompleteListener;
 import net.craftsupport.anticrasher.packet.WindowListener;
@@ -10,6 +13,7 @@ import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 
 public final class AntiCrasher extends JavaPlugin {
     private boolean isPAPIEnabled;
+    private static final String SPIGOT_RESOURCE_ID = "113404";
 
     @Override
     public void onLoad() {
@@ -32,6 +36,12 @@ public final class AntiCrasher extends JavaPlugin {
         PacketEvents.getAPI().getEventManager().registerListener(new WindowListener(this), PacketListenerPriority.LOWEST);
         PacketEvents.getAPI().getEventManager().registerListener(new TabCompleteListener(this), PacketListenerPriority.LOWEST);
         PacketEvents.getAPI().init();
+        new UpdateChecker(this, UpdateCheckSource.SPIGOT, SPIGOT_RESOURCE_ID)
+                .setUserAgent(new UserAgentBuilder().addPluginNameAndVersion())
+                .setNotifyByPermissionOnJoin("anticrasher.updatechecker")
+                .setNotifyOpsOnJoin(true)
+                .setDownloadLink("https://www.spigotmc.org/resources/anticrasher.113404/")
+                .checkNow();
     }
     @Override
     public void onDisable() {
