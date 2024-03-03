26d9dbbb216ad3b125139cc17c7a3922b1053621
MyoWalE <161393432+MyoWalE@users.noreply.github.com>
2024-03-03 11:24:10 +0900

    remove not used statements

--- src/main/java/net/smashyalts/anticrasher/utils/getBukkitVersion.java
+++ src/main/java/net/smashyalts/anticrasher/utils/getBukkitVersion.java
@@ -1,38 +1,7 @@
 package net.smashyalts.anticrasher.utils;
 
-import net.smashyalts.anticrasher.AntiCrasher;
 import org.bukkit.Bukkit;
-
-import java.util.logging.Level;
-
 public class getBukkitVersion {
-    // Error message when plugin doesn't support bukkit
-
-    public static boolean isMC120(){
-        return Bukkit.getBukkitVersion().contains("1.20");
-    }
-    public static boolean isMC119(){
-        return Bukkit.getBukkitVersion().contains("1.19");
-    }
-    public static boolean isMC118(){
-        return Bukkit.getBukkitVersion().contains("1.18");
-    }
-
-    public static boolean isMC117(){
-        return Bukkit.getBukkitVersion().contains("1.17");
-    }
-
-    public static boolean isMC116(){
-        return Bukkit.getBukkitVersion().contains("1.16");
-    }
-    public static boolean isMC115(){
-        return Bukkit.getBukkitVersion().contains("1.15");
-    }
-
-    public static boolean isMC114(){
-        return Bukkit.getBukkitVersion().contains("1.14");
-    }
-
     public static boolean isMC113(){
         return Bukkit.getBukkitVersion().contains("1.13");
     }
@@ -66,110 +35,10 @@ public class getBukkitVersion {
     public static boolean isMC15(){
         return Bukkit.getBukkitVersion().contains("1.5");
     }
-    public boolean isMC120OrNewer(){
-        if (isMC120())
-            return true;
-        else if (isMC119()||isMC118()||isMC117()||isMC116()||isMC115()||isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC119OrNewer(){
-        if (isMC119())
-            return true;
-        else if (isMC118()||isMC117()||isMC116()||isMC115()||isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC118OrNewer(){
-        if (isMC118())
-            return true;
-        else if (isMC117()||isMC116()||isMC115()||isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC117OrNewer(){
-        if (isMC117())
-            return true;
-        else if (isMC116()||isMC115()||isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC116OrNewer(){
-        if (isMC116())
-            return true;
-        else if (isMC115()||isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC115OrNewer(){
-        if (isMC115())
-            return true;
-        else if (isMC114()||isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC114OrNewer(){
-        if (isMC114())
-            return true;
-        else if (isMC113()||isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
     public static boolean isMC113OrNewer(){
         if (isMC113())
             return true;
         else if (isMC112()||isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC112OrNewer(){
-        if (isMC112()) {
-            return true;
-        } else if (isMC111()||isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC111OrNewer(){
-        if (isMC111()) {
-            return true;
-        } else if (isMC110()||isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC110OrNewer(){
-        if (isMC110()) {
-            return true;
-        } else if (isMC19()||isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-
-    public boolean isMC19OrNewer(){
-        if (isMC19()) {
-            return true;
-        } else if (isMC18()||isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-
-    public boolean isMC18OrNewer(){
-        if (isMC18()) {
-            return true;
-        } else if (isMC17()||isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC17OrNewer(){
-        if (isMC17()) {
-            return true;
-        } else if (isMC16()||isMC15())
-            return false;
-        return true;
-    }
-    public boolean isMC16OrNewer(){
-        if (isMC16()) {
-            return true;
-        } else if (isMC15())
             return false;
         return true;
     }
